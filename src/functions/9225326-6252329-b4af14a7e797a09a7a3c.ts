ea98ce379ee3ce48e8e4bb7af541e21242a77f99
Denny José Vigil Majano <sr.denny@wird.ai>
2023-11-28 13:05:18 -0600

    Flow Update

--- src/functions/9225326-6252329-b4af14a7e797a09a7a3c.ts
+++ src/functions/9225326-6252329-b4af14a7e797a09a7a3c.ts
@@ -3,16 +3,16 @@ import * as https from "https";
 import * as df from 'durable-functions';
 import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
 
-const orchestrator = () => {}
+codigo que reemplazó al antiguo
 
 export async function serviceBusQueueTrigger(message: unknown, context: InvocationContext): Promise<void> {
     context.log('Service bus queue function processed message:', message);
     const client = df.getClient(context);
-    const instanceId: string = await client.startNew("orchestrator", message);
+    const instanceId: string = await client.startNew("9225326-6252329-b4af14a7e797a09a7a3c", message);
     context.log(`Started orchestration with ID = '${instanceId}'.`);
 }
-app.serviceBusQueue('orchestrator', {
-    connection: 'azureQueueConnection',
+app.serviceBusQueue('9225326-6252329-b4af14a7e797a09a7a3c', {
+    connection: 'connection',
     queueName: '9225326-6252329-b4af14a7e797a09a7a3c',
     handler: serviceBusQueueTrigger,
     extraInputs: [df.input.durableClient()],
